c6081511e378d9fb0b3dc7cce6c32ae1d9e9ca45
David Felipe Castillo Velázquez <casvel@github.com>
2013-11-21 20:24:32 -0600

    Funciona llamar ventana Nueva Orden v 2.0

--- src/modules/NuevaOrden.py
+++ src/modules/NuevaOrden.py
@@ -2,17 +2,29 @@
 # -*- encoding: utf-8 -*-
 
 from gi.repository import Gtk
+global builder
 
 class Handler:
     def onDestroyWindow(self, *args):
         Gtk.main_quit(*args)
 
+    def AgregarButton_clicked(self, button):
+        global builder
+        folioEntry = builder.get_object("FolioEntry")
+        print(type(folioEntry))
+        folio = folioEntry.get_text()
+        print (folio)
+        print ("lala")
+
+    def CancelarButton_clicked(self, button):
+        pass
+
 class WinNuevaOrden:
     def __init__(self):
+        global builder
         builder = Gtk.Builder()
         builder.add_from_file("gui/NuevaOrden.glade")
         builder.connect_signals(Handler())
-
 
 def NuevaOrden():
     WinNuevaOrden()

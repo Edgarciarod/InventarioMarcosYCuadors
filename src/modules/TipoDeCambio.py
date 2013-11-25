99e4501b01d6160dbec066128ff8f5cfcdbc63c5
Edgar García Rodríguez <edgarciarod@gmail.com>
2013-11-25 15:42:33 -0600

    Completado Nuevo Pedido e iniciando tipo de cambio

---
+++ src/modules/TipoDeCambio.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env pyton3
+# -*- encoding: utf-8 -*-
+
+import time
+
+class TipoDeCambio:
+    def __init__(self):
+        self.fecha_actulizacion = None
+        self.pesos_to_dolares = None
+        self.dolares_to_pesos = None
+
